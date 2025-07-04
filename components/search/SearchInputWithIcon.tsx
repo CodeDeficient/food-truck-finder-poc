1b1aaac37d6cf5c62f6908363d244f869a5c3fb8
CodeAnt AI <support@codeant.ai>
2025-07-04 18:16:41 +0000

    changed files

--- components/search/SearchInputWithIcon.tsx
+++ components/search/SearchInputWithIcon.tsx
@@ -35,13 +35,13 @@ export function SearchInputWithIcon({
   placeholder,
 }: SearchInputWithIconProps) {
   return (
-    <div className="relative flex-grow min-w-[200px]">
-      <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 dark:text-gray-500 size-4" />
+    <div className="relative grow min-w-[200px]">
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 dark:text-gray-500 size-4" />
       <Input
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="pl-10 w-full dark:bg-slate-700 dark:text-gray-100 dark:placeholder-gray-400"
+        className="pl-10 w-full dark:bg-slate-700 dark:text-gray-100 dark:placeholder:text-gray-400"
         onKeyDown={onKeyDown}
       />
     </div>
