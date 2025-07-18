ab4d275dc5b295f5b92b9c62a7dc498ac68f3b16
CodeDeficient <zabrien@gmail.com>
2025-07-18 19:10:09 -0400

    feat: professional codebase cleanup - reduced root clutter by 55%

--- filter-eslint-errors.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const results = JSON.parse(fs.readFileSync('lint-results.json', 'utf8'));
-const strictBooleanErrors = results.flatMap(file => file.messages).filter(msg => msg.ruleId === '@typescript-eslint/strict-boolean-expressions');
-console.log(JSON.stringify(strictBooleanErrors, null, 2));
